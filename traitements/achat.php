4a882d76873f2742e9106e54ce6891491f48c3f8
djibygass <55426379+djibygass@users.noreply.github.com>
2021-04-07 10:32:11 +0200

    espace membre

--- traitements/achat.php
+++ traitements/achat.php
@@ -1,38 +1,14 @@
 <?php
-require_once('../modeles/Users.php');
-$customer = new Customers();
-if(isset($_POST['nom']) && !empty($_POST['nom']) &&
-    isset($_POST['prenom']) && !empty($_POST['prenom']) && 
-    isset($_POST['email']) && !empty($_POST['email']) &&
-    isset($_POST['adresse']) && !empty($_POST['adresse']) &&
-    isset($_POST['paiement']) && !empty($_POST['paiement'])  &&
-    isset($_POST['cvv']) && !empty($_POST['cvv'])){
-        
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $email = $_POST['email'];
-    $adresse = $_POST['adresse'];
-    $paiement = $_POST['paiement'];
-    $cvv = $_POST['cvv'];
+session_start();
+require_once "../modeles/Books.php";
+$books = new Books();
+if(isset($_POST['paiement']) && !empty($_POST['paiement']) && isset($_POST['cvv']) && !empty($_POST['cvv'])){
 
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-          header("location:../inscription.php?ERREUR1=TRUE");
-         }//else{
-        //   if(sizeof($customer->checkThisUser($login)) > 0){
-        //     header("location:../inscription.php?ERREUR2=TRUE");
-        //   }else{
-
-        //       $password = password_hash($password, PASSWORD_BCRYPT);
-        //       $customer->registerThisCustomer($nom,$prenom,$login,$password);
-        
-        //       header('location:../connexion.php');
-
-        //   }
-
-      
-        
-    }else{
-      header("location:../buyabook.php?ERREUR3=TRUE");
-    }
-
+   $books -> buyABook($_SESSION['idClient'],$_SESSION['idachat']);
+   
+   header("location:../espacemembre.php?success=true");
+}
+else{
+   header("location:../buyabook.php?idachat=".$_SESSION['idachat']."&erreur=true");
+}
 ?>

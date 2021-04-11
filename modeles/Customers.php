0b7b699872ff8bef141dbfd60ad14c789f727a62
djibygass <55426379+djibygass@users.noreply.github.com>
2021-04-11 23:36:48 +0200

    declaration de perte finish

--- modeles/Customers.php
+++ modeles/Customers.php
@@ -13,13 +13,12 @@ Class Customers extends Modele{
     $sql = "INSERT INTO clients(nom, prenom, login, mdp, adresse) VALUES (?, ?, ?, ?, ?)";
     return $this->executeRequest($sql,[$lastname,$firstname,$login,$password,$adresse]);
   }
+  //contact form -> support
   public function sendMessage(string $firstname, string $lastname, string $email,string $subject, string $message ){
-    $sql = "INSERT INTO messages(prenom, nom, email, sujet, contenu, date_time) VALUES (?,?,?,?,?,NOW())";
+    $sql = "INSERT INTO custumers_questions(prenom, nom, email, sujet, contenu, date_time) VALUES (?,?,?,?,?,NOW())";
     return $this->executeRequest($sql,[$firstname,$lastname,$email,$subject,$message]);
   }
-  // public function buyABook(string $firstname, string $lastname, string $email){
-
-  // }
+ 
 
 }
 
